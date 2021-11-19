57d65fa8670350ab0192efc3c081253f4a8590f0
daren-scerri <daren.scerri@mcast.edu.mt>
2021-11-19 12:52:36 +0100

    Enemies spawning using enemy list

---
+++ SuperCannon61B-2122/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+
+    [SerializeField] List<GameObject> myenemiesList;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        StartCoroutine(SpawnEnemies());
+    }
+
+
+    IEnumerator SpawnEnemies()
+    {
+
+        while (true)
+        {
+            int enemychoice = Random.Range(0, myenemiesList.Count );
+            float xpos = Random.Range(GameData.GetXMin(), GameData.GetXMax());
+
+            //instantiate a new gameobject from the loaded prefab at the top of the scene
+            Instantiate(myenemiesList[enemychoice], new Vector3(xpos, GameData.GetYMax() + 1, 0f), Quaternion.identity);
+            yield return new WaitForSeconds(Random.Range(0.2f, 2f));
+        }
+
+    }
+
+}
---
+++ SuperCannon61B-2122/Assets/Scripts/LargeBullet.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LargeBullet : Bullet
+{
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        speed = 3f;
+        base.Start();
+
+    }
+}
